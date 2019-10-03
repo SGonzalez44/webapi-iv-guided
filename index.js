a5cc5de9385e2e6cc6e1a331adf2d47af853895d
salvador <salgonzolanda@gmail.com>
2019-10-03 13:26:16 -0700

    made

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const server = require('./api/server.js');
+const express = require('express');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+const server = express();
+
+server.get('/', (req, res) => {
+  res.status(200).json({ hello: 'Node 11' });
 });
+
+//heroku will need a dynamic port
+//we can read the POPRT value by heroku to the environment
+
+const port = process.env.PORT || 4400;
+server.listen(port, () => console.log(`\n api running on ${port} \n`));
