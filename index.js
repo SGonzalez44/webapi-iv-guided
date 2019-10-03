6f67999a0efb93e87ef1300a41b2c9222fb2708c
salvador <salgonzolanda@gmail.com>
2019-10-03 14:15:02 -0700

    installed dotenv

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
-const server = express();
+require('dotenv').config(); // add this line as the first thing to run1
 
-server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Node 11' });
+const server = require('./api/server.js');
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
-
-//heroku will need a dynamic port
-//we can read the POPRT value by heroku to the environment
-
-const port = process.env.PORT || 4400;
-server.listen(port, () => console.log(`\n api running on ${port} \n`));
